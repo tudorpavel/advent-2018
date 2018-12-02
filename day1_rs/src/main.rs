daf12a830a154db3818a0acb1b7a482d41a8811f
Tudor Pavel <tudorv.pavel@gmail.com>
2018-12-02 20:41:09 +0200

    day1 in Rust

---
+++ day1_rs/src/main.rs
@@ -0,0 +1,39 @@
+use std::io;
+use std::io::prelude::*;
+use std::collections::HashMap;
+
+fn main() {
+    let mut nums = vec![];
+
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        let n: i32 = match line.unwrap().trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        nums.push(n);
+    }
+
+    println!("Part 1: {}", sum(&nums));
+    println!("Part 2: {}", find_repeat_frequency(&nums));
+}
+
+fn sum(nums: &Vec<i32>) -> i32 {
+    nums.iter().sum()
+}
+
+fn find_repeat_frequency(nums: &Vec<i32>) -> i32 {
+    let mut partial_sums = HashMap::new();
+    let mut sum = 0;
+
+    loop {
+        for num in nums.iter() {
+            sum += num;
+            if partial_sums.contains_key(&sum) {
+                return sum;
+            }
+            partial_sums.insert(sum, true);
+        }
+    }
+}
