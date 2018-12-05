076f756f5902512cb992491fcd00ab59a19c1969
Tudor Pavel <tudorv.pavel@gmail.com>
2018-12-05 19:32:37 +0200

    day5 - part 2

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -2,6 +2,7 @@ use std::io;
 use std::io::prelude::*;
 
 const DISTANCE_SAME_LETTER: i32 = (b'a' - b'A') as i32;
+const ALPHABET: &str = "abcdefghijklmnopqrstuvwxyz";
 
 fn main() {
     let mut lines = vec![];
@@ -12,13 +13,13 @@ fn main() {
         lines.push(s);
     }
 
-    let polymer = &lines[0];
+    let polymer = String::from(lines[0].clone());
 
     println!("Part 1: {}", solve_part1(&polymer));
-    // println!("Part 2: {}", solve_part2(&polymer));
+    println!("Part 2: {}", solve_part2(&polymer));
 }
 
-fn solve_part1(polymer: &str) -> usize {
+fn solve_part1(polymer: &String) -> usize {
     let mut stack: Vec<char> = Vec::new();
 
     for character in polymer.chars() {
@@ -37,6 +38,24 @@ fn solve_part1(polymer: &str) -> usize {
     stack.len()
 }
 
+fn solve_part2(polymer: &String) -> usize {
+    let mut min = polymer.len();
+
+    for letter in ALPHABET.chars() {
+        let result = solve_part1(&strip_letter(&polymer, &letter));
+
+        if result < min {
+            min = result
+        }
+    }
+
+    min
+}
+
 fn is_unit(c1: &char, c2: &char) -> bool {
     ((*c1 as i32) - (*c2 as i32)).abs() == DISTANCE_SAME_LETTER
+}
+
+fn strip_letter(polymer: &String, letter: &char) -> String {
+    polymer.chars().filter(|&c| c.to_lowercase().to_string() != letter.to_string()).collect()
 }
